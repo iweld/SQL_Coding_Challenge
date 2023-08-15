953a6faa20facd936701ed368922cc6f750fe9f5
Jaime M. Shaker <eyeweld@gmail.com>
2023-08-15 18:10:38 -0500

    Removing unecessary text and rewoding bash walkthough

--- source_data/scripts/start_fresh.sh
+++ source_data/scripts/start_fresh.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-echo "Drop All Tables"
 psql sql_coding_challenge -f drop_all_tables.sql
-echo "Executing build_tables.sql"
 psql sql_coding_challenge -f build_tables.sql
-echo "Executing normalize_tables.sql"
 psql sql_coding_challenge -f normalize_tables.sql
-echo "Executing remove_duplicates.sql"
 psql sql_coding_challenge -f remove_duplicates.sql
-echo "Executing cleanup_db.sql"
 psql sql_coding_challenge -f cleanup_db.sql
-echo "Executing create_relationships.sql"
 psql sql_coding_challenge -f create_relationships.sql
 echo "Build Process Complete"
