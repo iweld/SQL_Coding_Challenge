8145aabbd45e6a299b4415eb416c984b23bed808
Jaime M. Shaker <eyeweld@gmail.com>
2023-08-16 21:26:32 -0500

    Adding comments to bash script

--- source_data/scripts/start_fresh.sh
+++ source_data/scripts/start_fresh.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Although these commands can be done in a simple one-liner, I've made
+# the individual commands for new people that may not understand the syntax.
+#
+# The breakdown of the command are as follows.
+# psql: is a terminal-based front-end to PostgreSQL.
+# sql_coding_challenge: Name of our database.
+# -f: Checks if the file exists and is a regular file.
+# some_file.sql: The sql script  to execute via psql 
+
 psql sql_coding_challenge -f drop_all_tables.sql
 psql sql_coding_challenge -f build_tables.sql
 psql sql_coding_challenge -f normalize_tables.sql
